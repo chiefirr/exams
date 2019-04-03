ae93221eae554ff5d9f215ebf2d148cc24b5a140
andrii <andrii.isiuk@whiteaster.com>
2019-04-03 23:45:46 +0200

    Secondary tasks solved

---
+++ additional_tasks/skyphrases.py
@@ -0,0 +1,23 @@
+import os
+
+from additional_tasks.helpers import parse_file_content
+from exams.settings import BASE_DIR
+
+FILENAME = 'skychallenge_skyphrase_input.txt'
+FILEPATH = os.path.join(BASE_DIR, 'additional_tasks', 'files', FILENAME)
+
+
+def checker_unique(content):
+    ctr = 0
+    for line in content:
+        line_splited = line.split(' ')
+        ctr += len(set(line_splited)) == len(line_splited)
+    return ctr
+
+
+if __name__ == '__main__':
+    content = parse_file_content(FILEPATH)
+    result = checker_unique(content)
+
+    print("Len unique: ", result)
+    print("Len content: ", len(content))
