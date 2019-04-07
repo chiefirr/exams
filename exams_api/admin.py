3c93319cd3ea1e769df5740b78e7746c38d8a07d
andrii <andrii.isiuk@whiteaster.com>
2019-04-07 20:29:01 +0200

    Add admin to permissions

--- exams_api/admin.py
+++ exams_api/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from guardian.admin import GuardedModelAdmin
 
-# Register your models here.
+from exams_api.models import ExamSheet, TaskSheet, Exam, Task, MarksRange
+
+
+class ExamSheetAdmin(GuardedModelAdmin):
+    pass
+
+
+class TaskSheetAdmin(GuardedModelAdmin):
+    pass
+
+
+class ExamAdmin(GuardedModelAdmin):
+    pass
+
+
+class TaskAdmin(GuardedModelAdmin):
+    pass
+
+
+class MarksRangeAdmin(GuardedModelAdmin):
+    pass
+
+
+admin.site.register(ExamSheet, ExamSheetAdmin)
+admin.site.register(TaskSheet, TaskSheetAdmin)
+admin.site.register(Exam, ExamAdmin)
+admin.site.register(Task, TaskAdmin)
+admin.site.register(MarksRange, MarksRangeAdmin)
